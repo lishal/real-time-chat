69424e8c24bbed8e36208a16a31cbd5586e25d2a
lishal <52682738+lishal@users.noreply.github.com>
2024-02-18 20:37:52 +0545

    Verson 2.0 created"

--- Client/scripts/landing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("joinBtn").addEventListener("click", () => {
-  const name = document.getElementById("nameText").value;
-  if (name != "") {
-    document.getElementById("nameText").value = "";
-    window.location.href = "./chat.html?name=" + encodeURI(name);
-  }
-});
