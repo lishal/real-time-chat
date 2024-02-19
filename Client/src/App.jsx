d0f8c2f87b9cbd05258c16ec280432ec244be4f2
lishal <52682738+lishal@users.noreply.github.com>
2024-02-19 07:02:44 +0545

    React App created and ready to go

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import lianLogo from './assets/lian.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={lianLogo} alt="businessLogo"  />
+    </>
+  )
+}
+
+export default App
