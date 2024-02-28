4fcab1d26a500a0fbcad966bb09ff01380370d5c
lishal <52682738+lishal@users.noreply.github.com>
2024-02-28 08:25:56 +0545

    Linked with mongo db and all the apis are managed properly

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(
   cors({
     origin: "http://127.0.0.1:5173",
@@ -24,196 +25,152 @@ const io = new Server(server, {
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 4001;
 
-const users = {};
+const uri =
+  "mongodb+srv://lian:p6efeMzm9RJgCUhW@lian.b0zcvtj.mongodb.net/?retryWrites=true&w=majority&appName=Lian";
 
-io.on("connection", (socket) => {
-  const socketid = socket.id;
-  socket.on("newUser", (clientData) => {
-    // Add Active user in JSON
-    fs.readFile("userInRoom.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      try {
-        if (data != "") {
-          userInRoomData = JSON.parse(data);
-          if (clientData != null) {
-            const roomIndex = userInRoomData.findIndex(
-              (room) => room.roomId === clientData.roomId
-            );
-            if (roomIndex !== -1) {
-              const room = userInRoomData[roomIndex];
-              if (!room.activeUsers.some((user) => user.userId === socketid)) {
-                room.activeUsers.push({
-                  userId: socket.id,
-                  userName: clientData.name || clientData.owner,
-                });
-                fs.writeFile(
-                  "userInRoom.json",
-                  JSON.stringify(userInRoomData, null, 4),
-                  "utf8",
-                  (err) => {
-                    if (err) {
-                      console.error(err);
-                      return;
-                    }
-                  }
-                );
-              }
-            } else {
-              const data = {
-                roomId: clientData.roomId,
-                owner: clientData.owner,
-                roomName: clientData.roomName,
-                activeUsers: [
-                  {
-                    userId: socketid,
-                    userName: clientData.owner,
-                  },
-                ],
-              };
-              userInRoomData.push(data);
-              fs.writeFile(
-                "userInRoom.json",
-                JSON.stringify(userInRoomData, null, 5),
-                "utf8",
-                (err) => {
-                  if (err) {
-                    console.error(err);
-                    return;
-                  }
-                }
-              );
-            }
-          }
-        } else {
-          const data = {
-            roomId: clientData.roomId,
-            owner: clientData.owner || "Bot",
-            roomName: clientData.roomName || "Auto created Room",
-            activeUsers: [
-              {
-                userId: socketid,
-                userName: clientData.owner || clientData.name,
-              },
-            ],
-          };
-          fs.writeFile(
-            "userInRoom.json",
-            JSON.stringify([data], null, 5),
-            "utf8",
-            (err) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-            }
-          );
-        }
-      } catch (err) {
-        console.error("Error parsing JSON data:", err);
-      }
-    });
+async function connectToDatabase() {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    await client.connect();
+    return client.db("LIAN");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    throw err;
+  }
+}
 
-    // data.userId = socket.id;
-    // socket.broadcast.emit("userJoined", name);
-  });
-  socket.on("sendMessage", (message) => {
-    socket.broadcast.emit("receiveMessage", {
-      message: message,
-      name: users[socket.id],
-    });
-  });
-  socket.on("disconnect", () => {
-    fs.readFile("userInRoom.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      try {
-        if (data != "") {
-          const userData = JSON.parse(data);
-          const roomActiveMembers = userData.map((room) => ({
-            ...room,
-            activeUsers: room.activeUsers.filter(
-              (user) => user.userId !== socketid
-            ),
-          }));
-          fs.writeFile(
-            "userInRoom.json",
-            JSON.stringify(roomActiveMembers, null, 4),
-            "utf8",
-            (err) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-            }
-          );
-        }
-      } catch (err) {
-        console.error("Error parsing JSON data:", err);
-      }
-    });
-    //   socket.broadcast.emit("disconnectedUser", users[socket.id]);
-    //   delete users[socket.id];
-  });
-});
+async function addDataToCollection(db, collectionName, data) {
+  const collection = db.collection(collectionName);
+  try {
+    await collection.insertOne(data);
+  } catch (err) {
+    console.error(`Failed to add data to collection ${collectionName}`, err);
+    throw err;
+  }
+}
 
-app.post("/api/joinRoom", (req, res) => {
-  const clientData = req.body;
-  fs.readFile("userInRoom.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    try {
-      if (data != "") {
-        activeRoom = JSON.parse(data);
-        const filteredRoomCode = activeRoom.filter(
-          (element) => element.roomId === clientData.roomId
-        );
-        if (filteredRoomCode.length != 0) {
-          res.json(clientData);
-        } else {
-          res.status(400).json({ error: "Invalid roomcode" });
-        }
+async function findDataFromCollection(
+  db,
+  collectionName,
+  fieldName,
+  findValue
+) {
+  const collection = db.collection(collectionName);
+  const room = await collection.findOne({ [fieldName]: findValue });
+  return room;
+}
+async function countDocument(db, collectionName, fieldName, findValue) {
+  const collection = db.collection(collectionName);
+  const room = await collection.countDocuments({ [fieldName]: findValue });
+  if (room === 0) {
+    await deleteUser(db, "Room", fieldName, findValue);
+  }
+  return !!room;
+}
+async function checkExistOrNot(db, collectionName, fieldName, findValue) {
+  const collection = db.collection(collectionName);
+  const room = await collection.findOne({ [fieldName]: findValue });
+  return !!room;
+}
+async function deleteUser(db, collectionName, fieldName, deleteValue) {
+  const collection = db.collection(collectionName);
+  const room = await collection.deleteOne({ [fieldName]: deleteValue });
+  return !!room;
+}
+async function findAllRecords(db, collectionName, fieldName, fieldValue) {
+  const collection = db.collection(collectionName);
+  const records = await collection.find({ [fieldName]: fieldValue }).toArray();
+  return records;
+}
+async function cleanData(db) {
+  const collection = db.collection("Room");
+  const room = await collection.find().toArray();
+  room.forEach(async (individualRoom) => {
+    await countDocument(db, "RoomUserInfo", "roomId", individualRoom.roomId);
+  });
+}
+// Usage
+connectToDatabase()
+  .then(async (db) => {
+    const roomData = {};
+    const userData = {};
+    await cleanData(db);
+    app.post("/api/createRoom", async (req, res) => {
+      const data = req.body;
+      (roomData._id = Math.floor(Math.random() * 9999) + Date.now()),
+        (roomData.roomId = data.roomId),
+        (roomData.userId = data.userId),
+        (roomData.roomName = data.roomName);
+
+      const duplicateRoomCheck = await checkExistOrNot(
+        db,
+        "Room",
+        "roomId",
+        roomData.roomId
+      );
+      if (duplicateRoomCheck === false) {
+        await addDataToCollection(db, "Room", roomData);
+        res.json(data);
       } else {
-        res.status(400).json({ error: "Invalid roomcode" });
+        res.status(400).json({ error: "Room already exist!" });
       }
-    } catch (err) {
-      console.error("Error parsing JSON data:", err);
-    }
-  });
-});
+    });
 
-app.post("/api/createRoom", (req, res) => {
-  const roomData = req.body;
-  fs.readFile("userInRoom.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    if (data != "") {
-      try {
-        userinroomData = JSON.parse(data);
-        const filteredRoomCode = userinroomData.filter(
-          (element) => element.roomId === roomData.roomId
-        );
-        if (filteredRoomCode.length != 0) {
-          res.status(400).json({ error: "Room already exist!" });
-        } else {
-          res.json(roomData);
-        }
-      } catch (err) {
-        console.error("Error parsing JSON data:", err);
+    app.post("/api/joinRoom", async (req, res) => {
+      const clientData = req.body;
+      const roomExist = await checkExistOrNot(
+        db,
+        "Room",
+        "roomId",
+        clientData.roomId
+      );
+      if (roomExist === false) {
+        res.status(400).json({ error: "Invalid roomcode" });
+      } else {
+        res.json(clientData);
       }
-    } else {
-      res.json(roomData);
-    }
+    });
+    io.on("connection", async (socket) => {
+      const socketId = socket.id;
+      socket.on("newUser", async (clientData) => {
+        (userData._id = clientData.userId),
+          (userData.roomId = clientData.roomId),
+          (userData.userName = clientData.name);
+        userData.socketId = socketId;
+        await addDataToCollection(db, "RoomUserInfo", userData);
+        const peopleList = await findAllRecords(
+          db,
+          "RoomUserInfo",
+          "roomId",
+          userData.roomId
+        );
+        const roomInfo = await findDataFromCollection(
+          db,
+          "Room",
+          "roomId",
+          userData.roomId
+        );
+        socket.emit("userJoin", {
+          peopleList: peopleList,
+          roomInfo: roomInfo,
+        });
+        await cleanData(db);
+      });
+      socket.on("disconnect", async () => {
+        await deleteUser(db, "RoomUserInfo", "socketId", socketId);
+      });
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
   });
-});
+
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
